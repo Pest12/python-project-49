fce0cb8a39844df9a2afbc0e28a5f96591643ffc
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-06-07 19:53:58 +0500

    add logics and games

---
+++ brain_games/games/br_even.py
@@ -0,0 +1,9 @@
+from random import randint
+
+game_rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def game_data():
+    math_question = randint(0, 100)
+    current_answer = 'yes' if math_question % 2 == 0 else 'no'
+    return math_question, current_answer
+
---
+++ brain_games/logics.py
@@ -0,0 +1,21 @@
+import prompt
+
+
+def start_game(game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(game.game_rules)
+    current_round = 1
+    last_round = 3
+    while current_round <= last_round:
+        math_question, correct_answer = game.game_data()
+        print(f'Question: {math_question}')
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            return f"Let's try again, {name}!"
+        current_round += 1
+    return(f"Congratulations, {name}!")
