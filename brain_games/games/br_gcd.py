ba755569d441117f94e248aa87f1bfe758858bd8
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-06-16 16:11:51 +0500

    update br_gcd

--- brain_games/games/br_gcd.py
+++ brain_games/games/br_gcd.py
@@ -3,14 +3,18 @@ from random import randint
 GAME_RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def game_data():
-    num_1 = randint(1, 100)
-    num_2 = randint(1, 100)
-    math_question = f'{num_1} {num_2}'
+def get_gcd(num_1, num_2):
     while num_1 != num_2:
         if num_1 > num_2:
             num_1 = num_1 - num_2
         else:
             num_2 = num_2 - num_1
-    correct_answer = str(num_2)
+    return str(num_2)
+
+
+def game_data():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    math_question = f'{num_1} {num_2}'
+    correct_answer = get_gcd(num_1, num_2)
     return math_question, correct_answer
