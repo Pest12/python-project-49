d580d78dd50dddb54de4e78bf30608efcac96a4c
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-06-20 15:37:46 +0500

    update calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,15 +4,15 @@ GAME_RULES = 'What is the result of the expression?'
 
 
 def game_data():
-    operator = ["-", "+", "*"]
-    oper = choice(operator)
+    operators = ["-", "+", "*"]
+    operator = choice(operators)
     num_1 = randint(0, 30)
     num_2 = randint(0, 30)
-    math_question = f'{num_1} {oper} {num_2}'
-    if oper == "-":
-        correct_answer = str(num_1 - num_2)
-    elif oper == "+":
-        correct_answer = str(num_1 + num_2)
-    elif oper == "*":
-        correct_answer = str(num_1 * num_2)
-    return math_question, correct_answer
+    math_question = f'{num_1} {operator} {num_2}'
+    if operator == "-":
+        correct_answer = num_1 - num_2
+    elif operator == "+":
+        correct_answer = num_1 + num_2
+    elif operator == "*":
+        correct_answer = num_1 * num_2
+    return math_question, str(correct_answer)
